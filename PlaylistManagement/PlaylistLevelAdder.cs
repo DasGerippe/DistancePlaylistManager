bb4c7b187f8d77b078b6bdc2e9cedf200c7fef29
DasGerippe <Mathias_Beyer@gmx.net>
2024-12-08 08:31:20 +0100

    Add PlaylistLevelAdder.

---
+++ PlaylistManagement/PlaylistLevelAdder.cs
@@ -0,0 +1,43 @@
+﻿using DataClasses;
+
+namespace PlaylistManagement
+{
+    public class PlaylistLevelAdder
+    {
+        private readonly IReadOnlyCollection<GameMode> _GameModesByPreference = new List<GameMode>
+        {
+            GameMode.Sprint,
+            GameMode.Challenge,
+            GameMode.Stunt,
+            GameMode.ReverseTag,
+        }.AsReadOnly();
+
+        public void AddLevelsToPlaylist(Playlist playlist, IEnumerable<Level> levels, GameMode? gameMode, bool keepDuplicates)
+        {
+            levels = gameMode.HasValue
+                ? levels.Where(level => level.GameModes.Contains(gameMode.Value))
+                : levels.Where(level => level.GameModes.Count > 0);
+
+            IEnumerable<PlaylistLevel> levelsToAdd = levels
+                .Select(level => new PlaylistLevel
+                {
+                    GameMode = gameMode ?? GetPreferredGameMode(level),
+                    LevelName = level.Name,
+                    LevelPath = level.GetLevelPath(),
+                });
+
+            if (!keepDuplicates)
+                levelsToAdd = levelsToAdd.Distinct().Except(playlist.Levels);
+
+            foreach (PlaylistLevel playlistLevel in levelsToAdd)
+            {
+                playlist.Levels.Add(playlistLevel);
+            }
+        }
+
+        private GameMode GetPreferredGameMode(Level level)
+        {
+            return _GameModesByPreference.FirstOrDefault(gameMode => level.GameModes.Contains(gameMode));
+        }
+    }
+}
