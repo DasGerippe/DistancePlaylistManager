95dca2dd4d08d4271bdd198d79b7f69ebacc8fb5
DasGerippe <Mathias_Beyer@gmx.net>
2024-12-08 13:15:10 +0100

    Rework ConsoleApp to be a proper CLI with commands, options and arguments.

---
+++ DistancePlaylistManagerConsoleApp/CommandFactory.cs
@@ -0,0 +1,118 @@
+﻿using DataClasses;
+using DataStoring;
+using DistanceWorkshop;
+using PlaylistManagement;
+using System.CommandLine;
+
+namespace DistancePlaylistManagerConsoleApp
+{
+    internal sealed class CommandFactory
+    {
+        private readonly IPlaylistRepository _PlaylistRepository;
+        private readonly DistanceWorkshopClient _DistanceWorkshopClient;
+        private readonly PlaylistManager _PlaylistManager;
+
+        internal CommandFactory(
+            IPlaylistRepository playlistRepository,
+            DistanceWorkshopClient distanceWorkshopClient,
+            PlaylistManager playlistManager)
+        {
+            _PlaylistRepository = playlistRepository;
+            _DistanceWorkshopClient = distanceWorkshopClient;
+            _PlaylistManager = playlistManager;
+        }
+
+        internal RootCommand CreateCommands()
+        {
+            RootCommand rootCommand = new RootCommand(
+                description: "A tool for the racing game Distance that offers advanced playlist management features.");
+
+            Command collectionToPlaylistCommand = new Command(
+                name: "collection-to-playlist",
+                description: "Retrieves the levels from a workshop collection and adds them to a new or existing playlist.");
+
+            Option<string> collectionOption = new Option<string>(
+                name: "--collection",
+                description: "The url or steam id of the workshop collection.",
+                parseArgument: result =>
+                {
+                    if (result.Tokens.Count != 0)
+                        return result.Tokens[0].Value;
+
+                    result.ErrorMessage = "The '--collection' option must be set.";
+                    return string.Empty;
+                },
+                isDefault: true);
+
+            Option<string> playlistNameOption = new Option<string>(
+                name: "--playlist",
+                description: "The name of the playlist to which the levels are added. " +
+                             "If omitted, the collection name and game mode are used to generate a name.",
+                getDefaultValue: () => string.Empty);
+
+            Option<GameMode> gameModeOption = new Option<GameMode>(
+                name: "--gamemode",
+                description: "Only levels that support the specified game mode will be added to the playlist.",
+                getDefaultValue: () => GameMode.Sprint);
+
+            Option<bool> keepDuplicatesOption = new Option<bool>(
+                name: "--keep-duplicates",
+                description: "Levels will be added to the playlist, even if they are already included.");
+
+            collectionToPlaylistCommand.AddOption(collectionOption);
+            collectionToPlaylistCommand.AddOption(playlistNameOption);
+            collectionToPlaylistCommand.AddOption(gameModeOption);
+            collectionToPlaylistCommand.AddOption(keepDuplicatesOption);
+
+            collectionToPlaylistCommand.SetHandler(async (collection, playlist, gameMode, keepDuplicates) =>
+                {
+                    await AddCollectionLevelsToPlaylist(collection, playlist, gameMode, keepDuplicates);
+                },
+                collectionOption,
+                playlistNameOption,
+                gameModeOption,
+                keepDuplicatesOption);
+
+            rootCommand.AddCommand(collectionToPlaylistCommand);
+
+            return rootCommand;
+        }
+
+        private async Task AddCollectionLevelsToPlaylist(string collectionUrlOrId, string playlistName, GameMode gameMode, bool keepDuplicates)
+        {
+            try
+            {
+                WorkshopCollection collection = await GetWorkshopCollection(collectionUrlOrId);
+
+                if (string.IsNullOrEmpty(playlistName))
+                    playlistName = GeneratePlaylistName(collection.Name, gameMode);
+
+                Playlist playlist = _PlaylistManager.GetOrCreatePlaylist(playlistName);
+
+                PlaylistLevelAdder levelAdder = new PlaylistLevelAdder();
+                levelAdder.AddLevelsToPlaylist(playlist, collection.Levels, gameMode, keepDuplicates);
+
+                _PlaylistRepository.Update(playlist);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private async Task<WorkshopCollection> GetWorkshopCollection(string collectionUrlOrId)
+        {
+            bool isUrl = Uri.TryCreate(collectionUrlOrId, UriKind.Absolute, out Uri? collectionUrl);
+            return await (isUrl
+                ? _DistanceWorkshopClient.GetWorkshopCollection(collectionUrl!)
+                : _DistanceWorkshopClient.GetWorkshopCollection(collectionUrlOrId));
+        }
+
+        private string GeneratePlaylistName(string collectionName, GameMode? gameMode)
+        {
+            return gameMode == null
+                ? collectionName
+                : $"{collectionName} ({gameMode})";
+        }
+    }
+}
--- DistancePlaylistManagerConsoleApp/Program.cs
+++ DistancePlaylistManagerConsoleApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using DataClasses;
-using DataStoring;
+﻿using DataStoring;
 using DistanceWorkshop;
+using System.CommandLine;
+using PlaylistManagement;
 
 namespace DistancePlaylistManagerConsoleApp
 {
@@ -8,60 +9,40 @@ namespace DistancePlaylistManagerConsoleApp
     {
         private static readonly HttpClient HttpClient = new HttpClient();
 
-        static async Task Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("==== Distance Playlist Manager ====");
-            Console.WriteLine("Currently, the tool can only be used to create a playlist from a collection. ");
-            Console.WriteLine();
-
-            Console.WriteLine("Enter a URL or ID of a Distance workshop collection:");
-            string collectionUrlOrId = Console.ReadLine()!;
-            bool isUrl = Uri.TryCreate(collectionUrlOrId, UriKind.Absolute, out Uri? collectionUrl);
-
-            GameMode gameMode = ReadGameMode();
-
-            Console.WriteLine("Enter a name for the playlist (leave empty to use collection name):");
-            string playlistName = Console.ReadLine()!;
-
+            IPathsProvider pathsProvider = new DefaultPathsProvider();
+            IPlaylistRepository playlistRepository = new PlaylistFileRepository(pathsProvider);
+            PlaylistManager playlistManager = new PlaylistManager(playlistRepository);
             DistanceWorkshopClient distanceWorkshopClient = new DistanceWorkshopClient(HttpClient);
-            WorkshopCollection collection = await (isUrl
-                ? distanceWorkshopClient.GetWorkshopCollection(collectionUrl!)
-                : distanceWorkshopClient.GetWorkshopCollection(collectionUrlOrId));
 
-            Playlist playlist = new Playlist
+            CommandFactory commandFactory = new CommandFactory(playlistRepository, distanceWorkshopClient, playlistManager);
+            RootCommand rootCommand = commandFactory.CreateCommands();
+
+            if (args.Length > 0)
             {
-                Name = string.IsNullOrEmpty(playlistName) ? $"{collection.Name} ({gameMode})" : playlistName,
-                Levels = collection.Levels
-                    .Select(workshopLevel => new PlaylistLevel
-                    {
-                        GameMode = gameMode,
-                        LevelName = workshopLevel.Name,
-                        LevelPath = workshopLevel.GetLevelPath(),
-                    })
-                    .ToList(),
-            };
+                return await rootCommand.InvokeAsync(args);
+            }
 
-            PlaylistSerializer serializer = new PlaylistSerializer();
-            DefaultPathsProvider pathsProvider = new DefaultPathsProvider();
-            string playlistFilePath = pathsProvider.GetPlaylistFilePath(playlist.Name);
-            await using FileStream fileStream = File.Open(playlistFilePath, FileMode.Create, FileAccess.Write, FileShare.Read);
-            serializer.Serialize(fileStream, playlist);
-        }
-
-        private static GameMode ReadGameMode()
-        {
-            string possibleGameModes = string.Join(", ", Enum.GetNames<GameMode>());
-            Console.WriteLine($"Enter the game mode for the playlist ({possibleGameModes}):");
+            await rootCommand.InvokeAsync("--help");
             while (true)
             {
-                string? input = Console.ReadLine();
-                bool isGameModeString = Enum.TryParse(input, out GameMode gameMode);
+                string command = Console.ReadLine()!;
+                if (command.Equals("exit", StringComparison.CurrentCultureIgnoreCase) ||
+                    command.Equals("quit", StringComparison.CurrentCultureIgnoreCase))
+                    break;
 
-                if (isGameModeString)
-                    return gameMode;
-
-                Console.WriteLine("Invalid input! Try again:");
+                try
+                {
+                    await rootCommand.InvokeAsync(command);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
+
+            return 0;
         }
     }
 }
