534de43b51ed188b96ac1d35f72b65d3ec1c0875
DasGerippe <Mathias_Beyer@gmx.net>
2024-12-08 08:29:57 +0100

    Add PlaylistManager for advanced CRUD operations.

---
+++ PlaylistManagement/PlaylistManager.cs
@@ -0,0 +1,26 @@
+﻿using DataClasses;
+using DataStoring;
+
+namespace PlaylistManagement
+{
+    public sealed class PlaylistManager
+    {
+        private readonly IPlaylistRepository _PlaylistRepository;
+
+        public PlaylistManager(IPlaylistRepository playlistRepository)
+        {
+            _PlaylistRepository = playlistRepository;
+        }
+
+        public Playlist GetOrCreatePlaylist(string playlistName)
+        {
+            Playlist? playlist = _PlaylistRepository.GetAllPlaylists().FirstOrDefault(playlist => playlist.Name == playlistName);
+            if (playlist == null)
+            {
+                playlist = new Playlist { Name = playlistName };
+                _PlaylistRepository.Add(playlist);
+            }
+            return playlist;
+        }
+    }
+}
