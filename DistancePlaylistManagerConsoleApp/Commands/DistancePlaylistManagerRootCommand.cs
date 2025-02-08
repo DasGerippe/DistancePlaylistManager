afa2e0bcc892e7f70c8f6d4961bda3f4a5ad2872
DasGerippe <Mathias_Beyer@gmx.net>
2025-02-08 16:39:45 +0100

    Switch CollectionCommand and PlaylistCommand.

--- DistancePlaylistManagerConsoleApp/Commands/DistancePlaylistManagerRootCommand.cs
+++ DistancePlaylistManagerConsoleApp/Commands/DistancePlaylistManagerRootCommand.cs
@@ -10,8 +10,8 @@ namespace DistancePlaylistManagerConsoleApp.Commands
         internal DistancePlaylistManagerRootCommand(IPlaylistRepository playlistRepository) : base(
             description: "A tool for the racing game Distance that offers advanced playlist management features.")
         {
-            AddCommand(new PlaylistCommand(playlistRepository));
             AddCommand(new CollectionCommand(playlistRepository));
+            AddCommand(new PlaylistCommand(playlistRepository));
         }
     }
 }
